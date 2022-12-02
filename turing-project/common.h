972e6129b4d29d1752000c4b19013713ba486a86
jonathanli19 <quanhaoli2002@163.com>
2022-12-02 17:34:50 +0800

    main.cpp

---
+++ turing-project/common.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_H_
+#define __COMMON_H_
+
+#include <iostream>
+#include <string>
+#include <assert.h>
+
+#include <vector>
+
+using namespace std;
+
+#endif
---
+++ turing-project/main.cpp
@@ -0,0 +1,70 @@
+#include "common.h"
+const char* HELP_OUTPUT = "usage: turing [-v|--verbose] [-h|--help] <tm> <input>";
+const char* UNKNOWN_OPTION = "unknown option:";
+const char* UNKNOWN_COMMAND = "unknown command:";
+const char *END = "================== END ==================";
+int main(int argc, char* argv[])
+{
+    string tm_file = "";
+    string input_str = "";
+    bool verbose = false;
+    if(argc == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        int i = 1;
+        while(i < argc)
+        {
+            string arg = argv[i];
+            if(arg[0] == '-') //option
+            {
+                if(arg == "-h"||arg == "--help")
+                {
+                    cout << HELP_OUTPUT << endl;
+                    break;
+                }
+                else if(arg == "-v"||arg == "--verbose")
+                    verbose = true;
+                else
+                {
+                    cerr << UNKNOWN_OPTION << arg << endl;
+                    exit(0);
+                }
+            }
+            else//command
+            {
+                if(tm_file=="")
+                {
+                    if(arg.length() > 3 && arg.substr(arg.length()-3,arg.length())==".tm")
+                        tm_file = arg;
+                    else
+                    {
+                        cerr << UNKNOWN_COMMAND << arg << endl;
+                        exit(0);
+                    }
+                }
+                else if(input_str=="")
+                {
+                    input_str = arg;
+                }
+                else
+                {
+                    cerr << UNKNOWN_COMMAND << arg << endl;
+                    exit(0);
+                }
+            }
+            i++;
+        }
+    }
+    
+    TuringMachine tm(tm_file, verbose);
+    string res = tm.run(input_str);
+    if(verbose)
+        cout << "Result: ";
+    cout << res << endl;
+    if(verbose)
+        cout << END << endl;
+    return 0;
+}
